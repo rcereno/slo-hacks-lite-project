8376dab8598abf28c8ea3ce4c0855638deadfabb
npatel84 <77018379+npatel84@users.noreply.github.com>
2021-01-24 05:56:34 -0700

    Delete results.js

--- results.js
+++
@@ -1,8 +0,0 @@
-const finalScore = document.querySelector('#finalScore')
-const mostRecentScore = localStorage.getItem('mostRecentScore')
-
-
-const MAX_HIGH_SCORES = 5
-
-finalScore.innerText = mostRecentScore
-
